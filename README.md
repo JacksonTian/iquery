6992d850e906c46b0d86a9dda5c3cc982fa7be79
Jackson Tian <shyvo1987@gmail.com>
2012-12-27 17:02:06 +0800

    Refine and/or

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-where
+iQuery [![Build Status](https://travis-ci.org/JacksonTian/iquery.png?branch=master)](https://travis-ci.org/JacksonTian/iquery)
 =====
 
 select * from table where {}
+
--- test/sql.test.js
+++ test/sql.test.js
@@ -1,6 +1,7 @@
 var should = require('should');
 var SQL = require('../').SQL;
 var Order = require('../').Order;
+var Where = require('../').Where;
 
 describe('sql.js', function () {
   describe('SQL', function () {
@@ -26,6 +27,31 @@ describe('sql.js', function () {
       order.sort(['column2', true]);
       sql.select('*').from('table').where('1 = 1').groupBy('column1').orderBy(order).limit(100);
       sql.toString().should.equal('SELECT * FROM table WHERE 1 = 1 GROUP BY column1 ORDER BY column2 DESC LIMIT 0, 100');
+    });
+
+    it('should be ok case1', function () {
+      var sql = new SQL();
+      var where = new Where();
+      where.and({tag: 1});
+      sql.select('auction').from("trade").where(where).groupBy('auction').orderBy('count(order) DESC').limit(200);
+      sql.toString().should.equal('SELECT auction FROM trade WHERE tag = :tag GROUP BY auction ORDER BY count(order) DESC LIMIT 0, 200');
+    });
+
+    it('should be ok case2', function () {
+      var sql = new SQL();
+      var where = new Where();
+      var condition = new Where();
+      condition.or({
+        cid: {
+          $in: [1, 2, 3]
+        },
+        cid1: {
+          $in: [4, 5, 6]
+        }
+      }).bracket();
+      where.and({tag1: 1, tag2: 2}).and(condition.toString());
+      sql.select('auction').from("trade").where(where).groupBy('auction').orderBy('count(order) DESC').limit(200);
+      sql.toString().should.equal('SELECT auction FROM trade WHERE tag1 = :tag1 AND tag2 = :tag2 AND (cid IN (:cid__in) OR cid1 IN (:cid1__in)) GROUP BY auction ORDER BY count(order) DESC LIMIT 0, 200');
     });
   });
 });
